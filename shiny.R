65509aee3d1f1287e01c8516ac035bf623931060
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-07-24 10:44:46 +0200

    adds sidebar and input

--- shiny.R
+++ shiny.R
@@ -2,20 +2,48 @@ library(shinydashboard)
 # Create an empty header
 header <- dashboardHeader(
   # Create a notification drop down menu
-  dropdownMenu(type = "notifications",
-               notificationItem(
-                 text="The International Space Station is overhead!"
+  dropdownMenu(type = "messages",
+               messageItem(
+                 from="Lily",
+                 message="Finish this course!"
                ))
 )
-sidebar <- dashboardSidebar(  sidebarMenu(    menuItem("Data",             tabName = "data"    ),    menuItem("Dashboard",             tabName = "dashboard")  ))
-body <- dashboardBody()
+sidebar <- dashboardSidebar(  
+  sidebarMenu(   
+menuItem("Dashboard",       
+         tabName = "dashboard"),  
+menuItem("Data",tabName = "data"    )),
+selectInput(
+  inputId="molecule",
+  label = "Molecule",
+  choices = c("Acide valproique", "Carbamazepine")
+))
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(
+      tabName = "dashboard",
+      tabBox(
+        title="Table title",
+        tabPanel("Tab1", "Content for tab1"),
+        tabPanel("Tab2", "content for Tab2")
+      )
+      )
+  ),
+  textOutput("molecule")
+)
+
 
 ui <- dashboardPage(header = header,
                     sidebar =sidebar,
-                    body = dashboardBody()
+                    body = body
 )
 
-server <- function(input, output) {}
+server <- function(input, output) {
+  output$molecule <- renderPrint({
+    input$molecule
+  })
+}
 
 shiny::shinyApp(ui, server)
 
