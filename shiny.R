59eb5a39791edeb9dd98019fe3de4a884a1b96ee
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-08-11 17:05:03 +0200

    learned

--- shiny.R
+++ shiny.R
@@ -5,8 +5,10 @@ library(plotly)
 data = read.csv('IM.csv')
 
 ui <- fluidPage(
-  titlePanel("Baby Name Explorer"),
+  titlePanel("Really really random"),
+  
   theme = shinythemes::shinytheme("flatly"),
+  
   sidebarLayout(
     sidebarPanel (
       textInput('name', 'Enter Name', 'David'),
@@ -14,24 +16,41 @@ ui <- fluidPage(
               'Choose a sex',
                    selected = 'F',
                    choices = colnames(data)),
-      sliderInput('bins', 'Chose a scale', min=0, max=1, value=1 )
+      sliderInput('bins', 'Chose a scale', min=0, max=1, value=1 ),
+      numericInput('height','Select height' ,50),
+      actionButton('showHeight', 'Only now Count lol')
     ),
     mainPanel(
-  
+      textOutput("height_cm"),
+      
       tabsetPanel(
-        tabPanel('Sex',DT::DTOutput('sex')),
-        tabPanel('Dosage',plotly::plotlyOutput('dosage'),    actionButton('hi', 'Help'))
+        tabPanel('Sex',renderText(output$height)),
+        tabPanel('Dosage',plotly::plotlyOutput('dosage'),    
+        actionButton('hi', 'Help'))
         )
   )
 ))
 server <- function(input, output, session) {
+ 
+   output$height_cm <- renderText({
+     height <- reactiveFunct()
+     paste("Your height in centimeters is", height ,"cm")
+  })
+  
+   reactiveFunct <- eventReactive(input$showHeight,{
+     input$height * 2.45
+   } )
+  
   output$dosage <- plotly::renderPlotly({
     ggplot(data=data, aes(x=as.numeric(dosage), fill='steelblue')) + geom_histogram(alpha=input$bins)
   })
-  renderText(input$sex)
+  
   #output$sex <- DT::renderDT(data)
   observeEvent(input$hi,
               { showModal(modalDialog('Well.... hi'))})
+
 }
+
+
 shinyApp(ui = ui, server = server)
 
